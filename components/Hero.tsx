4e524517320efac2190ade41a7702a2d0ddc1f6e
Annie McMahon <108587699+AnnieMcMahon@users.noreply.github.com>
2025-01-21 15:07:05 -0500

    removed apostrophe and updated README file

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -35,7 +35,7 @@ export default function Hero() {
               Whether contributing to a software development team or using my analytical skills in data-driven projects, I am committed to delivering high-quality solutions that drive business growth and user satisfaction.
               <br />
               <br />
-              I am seeking a position where I can apply my technical skills and problem-solving abilities. Let's connect and explore how I can contribute to the success of your team.
+              I am seeking a position where I can apply my technical skills and problem-solving abilities. Let&apos;s connect and explore how I can contribute to the success of your team.
             </p>
           </div>
         </div>
